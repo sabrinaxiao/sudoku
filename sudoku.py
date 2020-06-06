8c5dabec98bd50930a5ccb1c0fea3a0409c68629
sabrinaxiao <sabrinaxiao01@gmail.com>
2020-06-05 21:12:41 -0400

    finished backtrack sudoku

--- sudoku.py
+++ sudoku.py
@@ -9,6 +9,36 @@ board = [
     [1,2,0,0,0,7,4,0,0],
     [0,4,9,2,0,6,0,0,7]
 ]
+
+def solve(bo):
+    find = find_empty(bo)
+    if not find:
+        return True
+    else:
+        row,col = find
+    for i in range(1,10):
+        if valid(bo, i, (row, col)):
+            bo[row][col] = i
+            if solve(bo):
+                return True
+            bo[row][col] = 0
+    return False
+
+def valid(bo,num,pos):
+    for i in range(len(bo[0])):
+        if bo[pos[0]][i] == num and pos[1] != i:
+            return False
+    for i in range(len(bo)):
+        if bo[i][pos[1]] == num and pos[0] != i:
+            return False
+    boxX = pos[1] //3 
+    boxY = pos[0]//3 
+    for i in range(boxY*3,boxY*3+3): 
+        for j in range(boxX*3,boxX*3+3):
+            if bo[i][j] == num and (i,j) != pos:
+                return False
+    return True
+
 def print_board(bo):
     for i in range(len(bo)):
         if i % 3 ==0 and i != 0:
@@ -19,10 +49,16 @@ def print_board(bo):
             if j == 8:
                 print(bo[i][j])
             else:
-                print(str(bo[i][i]) + " ",end="")
+                print(str(bo[i][j]) + " ",end="")
         
 def find_empty(bo):
     for i in range(len(bo)):
         for j in range(len(bo[i])):
             if bo[i][j] == 0:
                 return (i,j) #row,col
+    return None
+
+print_board(board)
+solve(board)
+print("________________________________")
+print_board(board)
